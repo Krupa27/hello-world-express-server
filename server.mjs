f44472311f1e296eb49fcf5cd123d58c9f5e9465
Krupa_Sagar <91311159+Krupa27@users.noreply.github.com>
2023-03-20 00:34:39 +0530

    hello world complete

---
+++ server.mjs
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
+
+app.get("/", (req, res) => {
+    console.log(req.ip);
+    res.send('Hello World!')
+})
